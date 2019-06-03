875b5cce108bd7fe1bbf78c0cbf987bdd8e9cf6c
Smisklas <niclas_v.johansson@maxiv.lu.se>
2019-06-03 15:40:20 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+
+from distutils.core import setup
+
+setup(
+    name='SPECIES-XPS',
+    version='0.1.0',
+    author='N. Johansson',
+    author_email='niclas_v.johansson@maxiv.lu.se',
+    packages=['tapxps'],
+    url='http://github.com/Smisklas/tapxps/',
+    license='LICENSE',
+    description='Library for the processing of TAPXPS data',
+    long_description=open('README.md').read(),
+    #install_requires=[
+     #   "datetime",
+     #   "numpy",
+     #   're',
+    #],
+)
